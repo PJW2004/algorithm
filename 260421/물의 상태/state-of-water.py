9237b10723d2c00df6587a3833c795ccc4b4d1d7
박정원 <59524278+PJW2004@users.noreply.github.com>
2026-04-21 16:44:54 +0900

    Title: 물의 상태, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/물의 상태/state-of-water.py
+++ 260421/물의 상태/state-of-water.py
@@ -6,7 +6,9 @@ token:str = input()
 try:
     n = int(token)
 except ValueError:
-    raise ValueError(f"정수를 입력해 주세요. 입력: {token!r}")
+    # raise ValueError(f"정수를 입력해 주세요. 입력: {token!r}")
+    # 깜빡
+    raise ValueError(f"정수를 입력해 주세요. 입력: {token!r}") from None
 
 if not -200<=n<=200:
     raise ValueError(f"제한 조건(-200~200)에 부합하지 않습니다. 입력: {n}")
